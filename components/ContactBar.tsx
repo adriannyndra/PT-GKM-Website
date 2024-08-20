51adb7cc54a9cb3d96140cde8f0924dc7ad0d8cb
adriannyndra <adriannyndra@gmail.com>
2024-08-20 20:38:51 +0700

    Added Contact Bar, moved Navbar CSS into a module file

--- components/ContactBar.tsx
+++ components/ContactBar.tsx
@@ -3,10 +3,10 @@ import { FaEnvelope, FaPhone, FaMapMarkerAlt } from 'react-icons/fa';
 
 const ContactBar = () => {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600">
       <div className="max-w-[1366px] w-full text-center">
 
-        <ul className="flex flex-wrap justify-center gap-4 text-xs">
+        <ul className="flex flex-wrap justify-center gap-4 text-xs h-[30px] text-white">
           <li className="flex items-center gap-2">
             <FaEnvelope className="text-gray-600 dark:text-gray-300" />
             grahasindo@outlook.com
@@ -20,7 +20,7 @@ const ContactBar = () => {
             Jl. Pucang Anom VII No. 52, Kec. Gubeng, Kota Surabaya
           </li>
         </ul>
-        
+
       </div>
     </div>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,82 +1,75 @@
 import React from 'react';
 import Link from 'next/link';
-import { Margarine } from 'next/font/google';
+import styles from './css-modules/Navbar.module.css'
 
 const Navbar = () => {
   const links = ['Beranda', 'Layanan', 'Galeri', 'Kontak', 'Karir'];
 
   return (
 
-    <nav style={styles.navbar}>
-        <div style={styles.container}>
-            <div style={styles.logo}>
-                <img src="/assets/logo.png" alt="Logo" style={styles.logoImage} />
+    <nav className={styles.navbar}>
+        <div className={styles.container}>
+            <div className={styles.logo}>
+                <img src="/assets/logo.png" alt="Logo" className={styles.logoImage} />
             </div>
-            <div style={styles.links}>
+            <div className={styles.links}>
                 {links.map((link, index) => (
-                <Link href={`/${link.toLowerCase()}`} key={index} style={styles.link}>
+                <Link href={`/${link.toLowerCase()}`} key={index} className={styles.link}>
                     {link}
                 </Link>
                 ))}
             </div>
-            <div style={styles.langContainer}>
-                <img src="\assets\united-kingdom.png" alt="English" style={styles.lang}/>
-                <img src="\assets\indonesia.png" alt="Indo" style={styles.lang}/>
+            <div className={styles.langContainer}>
+                <img src="\assets\united-kingdom.png" alt="English" className={styles.lang}/>
+                <img src="\assets\indonesia.png" alt="Indo" className={styles.lang}/>
             </div>
         </div>
     </nav>
   );
 };
 
-const styles = {
-    navbar: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: '10px 20px',
-        backgroundColor: '#fff',
-        borderBottom: '1px solid #eaeaea',
-        width: '100%', // Ensure the navbar spans the full width
-    },
-    container: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        width: '1366px',
-        margin: '0 auto',
-    },
-    logo: {
-        display: 'flex',
-        alignItems: 'center',
-    },
-    logoImage: {
-        height: '60px', // Increase logo size as needed
-    },
-    links: {
-        display: 'flex',
-        gap: '60px', // Space between links
-        // alignItems: 'center',
-        // flexGrow: 1, // Ensure links container grows to center align items
-        // justifyContent: 'center', // Center align links
-    },
-    link: {
-        padding: '10px 20px',
-        // alignItems: 'center',
-        // textDecoration: 'none',
-        // color: '#000',
-        // borderRadius: '20px',
-        // transition: 'background-color 0.3s ease',
-        // display: 'flex',
-  },
-    lang: {
-        width: '24px',
-        height: '24px',
-        // margin: '2px'
-    },
-    langContainer: {
-        display: 'flex',
-        alignItems: 'center',
-    }
-};
+// const styles = {
+//     navbar: {
+//         display: 'flex',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         padding: '10px 20px',
+//         backgroundColor: '#fff',
+//         borderBottom: '1px solid #eaeaea',
+//         width: '100%', // Ensure the navbar spans the full width
+//     },
+//     container: {
+//         display: 'flex',
+//         alignItems: 'center',
+//         justifyContent: 'space-between',
+//         width: '1366px',
+//         margin: '0 auto',
+//     },
+//     logo: {
+//         display: 'flex',
+//         alignItems: 'center',
+//     },
+//     logoImage: {
+//         height: '50px', // Increase logo size as needed
+//     },
+//     links: {
+//         display: 'flex',
+//         gap: '60px', // Space between links
+
+//     },
+//     link: {
+//         padding: '10px 20px',
+
+//   },
+//     lang: {
+//         width: '24px',
+//         height: '24px',
+
+//     },
+//     langContainer: {
+//         display: 'flex',
+//         alignItems: 'center',
+//     }
+// };
 
 export default Navbar;
